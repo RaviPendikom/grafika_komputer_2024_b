39813c31ec85c73f5866bab3d819c63530268d18
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-10-23 16:32:20 +0800

    simple animation

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   createCanvas(600, 600);
+  background(78, 19, 187);
 }
 
 function draw() {
-  background(78, 19, 187);
-  fill(222, 90, 190, 200);
+  fill(random(255), random(255), random(255), random(255));
   noStroke();
-  circle(300, 300, 150);
+  circle(random(600), random(600), random(100));
 }
