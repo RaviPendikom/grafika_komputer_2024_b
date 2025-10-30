d9321a8d3abc18027128df29c9e6427256fd15b6
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-10-30 16:10:31 +0800

    Event Intro

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
+// User defined variable
+// EVENT
 function setup() {
   createCanvas(600, 600);
+  background(78, 19, 187);
 }
 
 function draw() {
-  background(78, 19, 187);
-  fill(222, 90, 190, 200);
-  noStroke();
-  circle(300, 300, 150);
+  // fill(222, 90, 190, 200);
+  rectMode(CENTER);
+  square(mouseX, mouseY, 100);
 }
